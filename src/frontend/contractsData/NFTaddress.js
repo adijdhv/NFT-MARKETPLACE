0bfa288d693db98189128be0dbc2406529738224
Aadesh Jadhav <55881613+adijdhv@users.noreply.github.com>
2023-01-07 01:55:55 +0530

    Add files via upload

---
+++ src/frontend/contractsData/NFTaddress.js
@@ -0,0 +1,6 @@
+
+ 
+  
+export const NFTaddress = "0xC72A06Ff0cFA07BDe77605A359099d8b1b081F27"
+  //"EXPLORER": "https://testnet.snowtrace.io/address/0xc72a06ff0cfa07bde77605a359099d8b1b081f27#code"
+ 
---
+++ src/frontend/contractsData/marketplace.js
@@ -0,0 +1,4 @@
+ 
+export const marketplaceaddress = "0x097a1903EA1ae6C5329e32600ECf2FcfEE7a6A88"
+ // "EXPLORER" :"https://testnet.snowtrace.io/address/0x097a1903ea1ae6c5329e32600ecf2fcfee7a6a88#code"
+ 
